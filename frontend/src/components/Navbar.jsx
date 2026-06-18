4dbd6316c0e63ade94243e125930aee9767cfc18
John Rocha <xjohnfitcodes@gmail.com>
2026-06-18 11:40:07 -0400

    feat: add responsive navbar with theme toggle and integrate react-icons

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,34 @@
+import "../styles/navbar.css";
+import { useState } from "react";
 import { Link, NavLink } from "react-router";
+import { IoIosMoon } from "react-icons/io";
+import { HiOutlineBars3 } from "react-icons/hi2";
+import { AiOutlineClose } from "react-icons/ai";
 
 const Navbar = () => {
-  return (
-    <nav>
-        <div className="container nav__container">
-            <Link to="/" className="nav__logo">ELECTIONS APP</Link>
-            <div>
-                <menu>
-                    <NavLink to="/elections">Elections</NavLink>
-                    <NavLink to="/results">Results</NavLink>
-                    <NavLink to="/logout">Logout</NavLink>
-                </menu>
-                
-            </div>
-        </div>
-    </nav>
-  )
-}
 
-export default Navbar
+    const [showNav, setShowNav] = useState(false);
+
+    return (
+        <nav>
+            <div className="container nav__container">
+                <Link to="/" className="nav__logo">ELECTIONS APP</Link>
+                <div>
+                    {
+                        showNav && (
+                            <menu>
+                                <NavLink to="/elections">Elections</NavLink>
+                                <NavLink to="/results">Results</NavLink>
+                                <NavLink to="/logout">Logout</NavLink>
+                            </menu>
+                        )
+                    }
+                    <button className="theme__toggle-btn"><IoIosMoon /></button>
+                    <button className="nav__toggle-btn" onClick={() => setShowNav(!showNav)}>{showNav ? <AiOutlineClose /> : <HiOutlineBars3 />}</button>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
