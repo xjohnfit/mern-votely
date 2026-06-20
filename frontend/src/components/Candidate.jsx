f9db0a0f10071379b59f7af0824c835c81e69a58
John Rocha <xjohnfitcodes@gmail.com>
2026-06-20 13:08:02 -0400

    feat: implement candidates page with candidate listing and styling

---
+++ frontend/src/components/Candidate.jsx
@@ -0,0 +1,13 @@
+const Candidate = ({ image, id, fullName, motto }) => {
+  return (
+    <article className="candidate">
+        <div className="candidate__image">
+            <img src={image} alt={fullName} />
+        </div>
+        <h5>{fullName?.length > 20 ? fullName.substring(0, 20) + '...' : fullName}</h5>
+        <small>{motto?.length > 25 ? motto.substring(0, 25) + '...' : motto}</small>
+        <button className="btn primary">Vote</button>
+    </article>
+  )
+}
+export default Candidate
--- frontend/src/pages/Candidates.jsx
+++ frontend/src/pages/Candidates.jsx
@@ -1,7 +1,28 @@
-const Candidates = () => {
-  return (
-    <div>Candidates</div>
-  )
-}
+import { useParams } from 'react-router';
+import { candidates as dummyCandidates } from '../data/data.js'
+import Candidate from '../components/Candidate';
+import '../styles/candidates.css';
 
-export default Candidates
+const Candidates = () => {
+
+    const { id }  = useParams();
+
+    // Get candidates that belong a specific election
+    const candidates = dummyCandidates.filter(candidate => candidate.electionId === Number(id));
+
+    return (
+        <section className="candidates">
+            <header className="candidates__header">
+                <h1>Vote your candidate</h1>
+                <p>This are the candidates for the selected election. Please vote wisely, you won't be allowed to vote in this election again.</p>
+            </header>
+            <div className="container candidates__container">
+                {
+                    candidates.map(candidate => <Candidate key={candidate.id} {...candidate} />)
+                }
+            </div>
+        </section>
+    );
+};
+
+export default Candidates;
