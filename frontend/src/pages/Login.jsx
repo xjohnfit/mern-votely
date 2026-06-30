9201058beaf87ba66339455c8f155655f6057bc0
John Rocha <xjohnfitcodes@gmail.com>
2026-06-30 10:50:49 -0400

    feat: integrate Axios for API requests in login and registration forms, enhance error handling and input management

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,38 +1,52 @@
 import { Link } from 'react-router';
-import "../styles/register.css";
+import '../styles/register.css';
 import { useState } from 'react';
 
 const Login = () => {
-
-    const [userData, setUserData] = useState(
-        {
-            email: '',
-            password: '',
-        }
-    );
+    const [userData, setUserData] = useState({
+        email: '',
+        password: '',
+    });
 
     // Function to change controlled input
     const inputHandler = (e) => {
         setUserData((prevState) => {
-            return { ...prevState, [e.target.name]: e.target.value}
+            return { ...prevState, [e.target.name]: e.target.value };
         });
-    }
+    };
 
     return (
-        <section className="register">
-            <div className="container register__container">
+        <section className='register'>
+            <div className='container register__container'>
                 <h2>Login</h2>
                 <form>
-                    <p className="form__error-message">
+                    <p className='form__error-message'>
                         Error from the backend
                     </p>
-                    <input onChange={inputHandler} type="email" name="email" placeholder="Email Address" autoComplete="true" />
-                    <input onChange={inputHandler} type="password" name="password" placeholder="Password" autoComplete="true" />
+                    <input
+                        onChange={inputHandler}
+                        type='email'
+                        name='email'
+                        placeholder='Email Address'
+                        autoComplete='true'
+                        autoFocus
+                    />
+                    <input
+                        onChange={inputHandler}
+                        type='password'
+                        name='password'
+                        placeholder='Password'
+                        autoComplete='true'
+                    />
                     <p>
-                        Don't have an account? {' '}
+                        Don't have an account?{' '}
                         <Link to='/register'>Register Now</Link>
                     </p>
-                    <button type='submit' className="btn primary">Login</button>
+                    <button
+                        type='submit'
+                        className='btn primary'>
+                        Login
+                    </button>
                 </form>
             </div>
         </section>
